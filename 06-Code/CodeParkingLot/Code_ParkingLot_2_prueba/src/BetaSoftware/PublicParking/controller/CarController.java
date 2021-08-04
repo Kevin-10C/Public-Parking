d4d4f10a96746537232d49795ab45af117c162cc
unknown <kevincriollo2997_@hotmail.com>
2021-08-03 19:19:06 -0500

    car Controller

--- 06-Code/CodeParkingLot/Code_ParkingLot_2_prueba/src/BetaSoftware/PublicParking/controller/CarController.java
+++ 06-Code/CodeParkingLot/Code_ParkingLot_2_prueba/src/BetaSoftware/PublicParking/controller/CarController.java
@@ -5,10 +5,44 @@
  */
 package BetaSoftware.PublicParking.controller;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author Eliana Cuaspa Beta-SoftwareTech ESPE-DCCO
  */
 public class CarController {
-    
+
+    public static String CheckInTime() {
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        Date date = new Date();
+        String newDate = dateFormat.format(date);
+        return newDate;
+    }
+
+    public static String DepartureTime() {
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        Date date = new Date();
+        String date2 = dateFormat.format(date);
+        return date2;
+    }
+
+    public static long calculeTime(String checkInTime, String departureTime) {
+        Date d1 = null;
+        Date d2 = null;
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+        try {
+            d1 = format.parse(checkInTime);
+            d2 = format.parse(departureTime);
+        } catch (ParseException e) {
+        }
+
+        long diff = d2.getTime() - d1.getTime();
+        long diffMinutes = diff / (60 * 1000);
+        return diffMinutes;
+
+    }
 }
